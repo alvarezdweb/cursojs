494bec060b9c06828ef0bccf230b468a34821cc7
Matias Alvarez <alvarez.dweb@gmail.com>
2020-09-29 10:00:06 -0300

    mobile + desktop

--- js/burgerbtn.js
+++ js/burgerbtn.js
@@ -8,12 +8,13 @@
 
         function openNav(estado) {
           if (estado == 0) {
-            document.getElementById("menu__bar__links").style.transform = "translateX(0)";
+            document.getElementById("menu__bar__links").style.transform = "translateY(0)";
             document.getElementById("menu__bar__links").style.transition = "all .5s";
+            
     
             window.estado++;
           } else if (estado == 1) {
-            document.getElementById("menu__bar__links").style.transform = "translateY(-190px)";
+            document.getElementById("menu__bar__links").style.transform = "translateY(-160px)";
             document.getElementById("menu__bar__links").style.transition = "all .5s";
             window.estado--;
           }
@@ -21,13 +22,23 @@
 
         function myFunction(x) {
           if (x.matches) {
-            document.getElementById("menu__bar__links").style.transform = "translateX(0)";
-            document.getElementById("menu__bar__links").style.boxShadow = "0px 0px 30px rgba(127, 137, 161, 0.5)";
+            document.getElementById("menu__bar__links").style.transform = "translateY(0)";
             document.getElementById("menu__bar__links").style.transition = "transform 0s";
+
+            
     
           } else {
-            document.getElementById("menu__bar__links").style.transform = "translateX(-250px)";
-            document.getElementById("menu__bar__links").style.boxShadow = "none";
+            document.getElementById("menu__bar__links").style.transform = "translateY(-160px)";
             document.getElementById("menu__bar__links").style.transition = "transform 0s";
+
+            
           }
         }
+    
+        var x = window.matchMedia("(min-width: 768px)")
+        myFunction(x)
+        x.addListener(myFunction)
+
+  
+
+   

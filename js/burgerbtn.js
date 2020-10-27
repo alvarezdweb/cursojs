16c3f63e11f50b48b877aa1d0265785ba67458be
Matias Alvarez <alvarez.dweb@gmail.com>
2020-10-27 16:42:42 -0300

    menu media query

--- js/burgerbtn.js
+++ js/burgerbtn.js
@@ -6,24 +6,29 @@
         const mQuery = () => {
           if($(window).width()>=768){
             menuLinks.css('transform','translateY(0px)')
-            menuLinks.css('transition','0s')
+            menuLinks.css('transition','transform 0s')
+            menuButton.removeClass('is-active');
 
           }else{
             menuLinks.css('transform','translateY(-160px)')
-            menuLinks.css('transition','0s')
+            menuButton.removeClass('is-active');
+            menuLinks.css('transition','transform 0s')
+
+
           }
         }
 
         const burgenBtn = () => {
           if(menuButton.hasClass('is-active')){
             menuLinks.css('transform','translateY(-160px)')
-            menuLinks.css('transition','.3s')
             menuButton.removeClass('is-active');
+            menuLinks.css('transition','transform .3s')
+
           }else{
             menuLinks.css('transform','translateY(0px)')
-            menuLinks.css('transition','.3s')
-
             menuButton.addClass('is-active');
+            menuLinks.css('transition','transform .3s')
+
           }
         }
         $(document).ready(mQuery);
